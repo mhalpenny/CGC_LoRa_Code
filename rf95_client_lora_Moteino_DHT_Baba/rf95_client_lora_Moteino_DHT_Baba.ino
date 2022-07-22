96dd7e3fdac815fd5a7e06d12d7fe17359d94e90
Matthew Halpenny <matt.halpenny@gmail.com>
2022-07-22 13:42:14 -0400

    init

---
+++ rf95_client_lora_Moteino_DHT_Baba/rf95_client_lora_Moteino_DHT_Baba.ino
@@ -0,0 +1,98 @@
+// rf95_client.pde
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messageing client
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example rf95_server
+// Tested with Anarduino MiniWirelessLoRa, Rocket Scream Mini Ultra Pro with
+// the RFM95W, Adafruit Feather M0 with RFM95
+
+#include <SPI.h>
+#include <RadioHead.h>
+#include <RH_RF95.h>
+#include <DFRobot_DHT11.h>
+
+int count = 0;
+uint8_t temperature; // test init to 10, uint8_t insure 8 bits length (i.e. unsigned char)
+uint8_t humidity; // test init to 15
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(A2, 9); //SPI CS init with MoteinoM0
+// Need this on Arduino Zero with SerialUSB port (eg RocketScream Mini Ultra Pro)
+//#define Serial SerialUSB
+DFRobot_DHT11 DHT;
+#define DHT11_PIN A1
+
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.println("Connecting..."); 
+  //while (!Serial) ; // Wait for serial port to be available
+  if (!rf95.init()){
+    Serial.println("init failed");
+  }else { 
+      Serial.print("init OK - ");
+  } 
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // You can change the modulation parameters with eg
+  // rf95.setModemConfig(RH_RF95::Bw500Cr45Sf128);
+  #define RF95_FREQ 915
+  rf95.setFrequency(RF95_FREQ);
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 2 to 20 dBm:
+   rf95.setTxPower(13, false);
+}
+
+void loop()
+{
+  Serial.println(count);
+  Serial.println("Preparing to send to rf95_server");
+  //compiling sensor data
+  Serial.println("Compiling sensor data...");
+//  here we would read sensors data [Baba]
+  //Serial.println(temperature); //[Baba]
+  //Serial.println(humidity); //[Baba]
+  DHT.read(DHT11_PIN);
+  delay(2000);
+  temperature = DHT.temperature; // test init to 10, uint8_t insure 8 bits length (i.e. unsigned char)
+  humidity = DHT.humidity; // test init to 15
+  // temperature = 10; // test init to 10, uint8_t insure 8 bits length (i.e. unsigned char)
+  // humidity = 15; // test init to 15
+  // Send a message to rf95_server
+  uint8_t data[] = {temperature, humidity}; //we want to send an array of two bytes [Baba]
+  //Serial.print("data "); Serial.print(data[0]); Serial.print(" "); Serial.println(data[1]);  
+  //data[0] = temperature; // first byte is the temperature measurement [Baba]
+  //data[1] = humidity; // seond byte is the humidity measurement [Baba]
+  rf95.send(data, sizeof(data));
+    Serial.println("Message sent");
+  rf95.waitPacketSent();
+  count++;
+  delay(5000);
+//  // Now wait for a reply
+ uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+ uint8_t len = sizeof(buf);
+//
+ if (rf95.waitAvailableTimeout(5000))
+ { 
+   // Should be a reply message for us now   
+   if (rf95.recv(buf, &len))
+  {
+     Serial.print("got reply: ");
+     Serial.println((char*)buf);
+//      Serial.print("RSSI: ");
+//      Serial.println(rf95.lastRssi(), DEC);    
+   }
+   else
+   {
+     Serial.println("recv failed");
+   }
+ }
+ else
+ {
+   Serial.println("No reply, is rf95_server running?");
+ }
+ delay(400);
+}
---
+++ rf95_server_lora_RFGateway_Baba/rf95_server_lora_RFGateway_Baba.ino
@@ -0,0 +1,68 @@
+// rf95_server.pde
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messageing server
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95  if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example rf95_client
+
+#include <SPI.h>
+#include <RadioHead.h>
+#include <RH_RF95.h>
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(20, 18); //RFGateway
+
+void setup() {   
+  Serial.begin((115200));
+  Serial.println("Connecting..."); 
+  while (!Serial) ; // Wait for serial port to be available
+  if (!rf95.init()){
+    Serial.println("init failed"); 
+    }else { 
+      Serial.print("init OK - ");
+      rf95.setModeRx();
+      Serial.print("Rx mode on");
+  } 
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  #define RF95_FREQ 915
+  rf95.setFrequency(RF95_FREQ);
+  // You can change the modulation parameters with eg
+  // rf95.setModemConfig(RH_RF95::Bw500Cr45Sf128);
+
+// The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 2 to 20 dBm:
+ rf95.setTxPower(13, false);
+}
+
+void loop(){
+  // Serial.println("Listening..."); 
+  if (rf95.available()){
+      Serial.println("Checking messages"); 
+    // Should be a message for us now   
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    if (rf95.recv(buf, &len)){
+        Serial.println("Mesage found");
+      // we know that for one message, the firt byte is temperature data and the second is humidity data [Baba]
+      Serial.print("humidity: "); //[Baba]
+      Serial.println(buf[0]); //[Baba]
+      Serial.print("temperature: "); //[Baba]
+      Serial.println(buf[1]); //[Baba]
+
+    //  Serial.print("RSSI: ");
+    //  Serial.println(rf95.lastRssi(), DEC);
+      
+      //Send a reply
+//      uint8_t data[] = "Oh hello";
+//      rf95.send(data, sizeof(data));
+//      rf95.waitPacketSent();
+//      Serial.println("Sent a reply");
+    }
+    else{
+      Serial.println("recv failed");
+    }
+  }
+}
